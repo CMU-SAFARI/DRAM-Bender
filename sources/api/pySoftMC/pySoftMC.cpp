47f9b1a862ca15a2e10af56456e3c7ec52014db2
Gus Smith <gus@normalcomputing.ai>
2026-05-29 11:11:40 -0700

    Use system include for pybind

--- sources/api/pySoftMC/pySoftMC.cpp
+++ sources/api/pySoftMC/pySoftMC.cpp
@@ -2,7 +2,7 @@
 #include "instruction.h"
 #include "prog.h"
 
-#include "ext/pybind11/include/pybind11/pybind11.h"
+#include <pybind11/pybind11.h>
 namespace py = pybind11;
 
 PYBIND11_MODULE(pySoftMC, m) 
